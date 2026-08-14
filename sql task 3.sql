389c09893691b86e47d8a0b3e5cc0dd922c43278
thusharmalli72-spec <thusharmalli72@gmail.com>
2026-08-14 11:43:29 +0530

    Add files via upload

---
+++ sql task 3.sql
@@ -0,0 +1,65 @@
+use thu_databse;
+CREATE TABLE students (
+    Student_ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Course VARCHAR(20),
+    Age INT,
+    Marks INT,
+    City VARCHAR(50)
+);
+SELECT* from students;
+
+update students
+set Course = "B.com (AI)"
+where Student_ID = 1;
+SELECT* from students;
+update students
+set Course = "B.com (DA)"
+where Student_ID = 2;
+SELECT* from students
+update students
+set Age = 1
+where update students;
+set Course = "B.com (AI)"
+where Student_ID = 1;
+SELECT* from stude = 20;
+SELECT* from students;
+
+update students
+set Course="BBA(AI)"
+where student_ID=3;
+
+update students
+set Course="fastion(+3)"
+where student_ID=4;
+select * from students;
+
+update students 
+set Course="bse(AI)"
+where Student_ID=5;
+select * from students;
+
+update students
+set City="bcroad"
+where Student_ID=1;
+select * from students;
+
+delete from students
+where Student_ID=1;
+select * from students;
+
+delete from students
+where Student_ID=2;
+select * from students;
+
+delete from students
+where Student_ID=3;
+select * from students;
+
+delete from students
+where Student_ID=4;
+select * from students;
+
+delete from students
+where Student_ID=5;
+select * from students;
